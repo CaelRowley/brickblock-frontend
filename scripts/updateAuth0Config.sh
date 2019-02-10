1e5db95853450a6b6fb0e904caecd0a7114ad937
Cael Rowley <caelrowley@gmail.com>
2019-02-10 17:07:39 +0000

    Deployment test

--- scripts/updateAuth0Config.sh
+++ scripts/updateAuth0Config.sh
@@ -4,6 +4,7 @@ echo $PWD
 cd ./src/Auth/
 echo $PWD
 
+rm -f auth0-variables-prod.js
 echo "export const AUTH_CONFIG = {" > auth0-variables-prod.js
 echo "domain: $AUTH0_DOMAIN," >> auth0-variables-prod.js
 echo "clientId: $AUTH0_CLIENT_ID," >> auth0-variables-prod.js
