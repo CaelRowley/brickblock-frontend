345459ed4884875cdaf2166268b405f733c50ad6
Cael Rowley <caelrowley@gmail.com>
2019-02-10 17:04:37 +0000

    Deployment test

--- scripts/updateAuth0Config.sh
+++ scripts/updateAuth0Config.sh
@@ -4,7 +4,9 @@ echo $PWD
 cd ./src/Auth/
 echo $PWD
 
-sed -i -e "s/process.env.AUTH0_DOMAIN/$AUTH0_DOMAIN/g" ./auth0-variables.js
-sed -i -e "s/process.env.AUTH0_CLIENT_ID/$AUTH0_CLIENT_ID/g" ./auth0-variables.js
-sed -i -e "s/process.env.AUTH0_CLIENT_SECRET/$AUTH0_CLIENT_SECRET/g" ./auth0-variables.js
-sed -i -e "s/process.env.AUTH0_CALLBACK_URL/$AUTH0_CALLBACK_URL/g" ./auth0-variables.js
+echo "export const AUTH_CONFIG = {" > auth0-variables-prod.js
+echo "domain: $AUTH0_DOMAIN," >> auth0-variables-prod.js
+echo "clientId: $AUTH0_CLIENT_ID," >> auth0-variables-prod.js
+echo "clientSecret: $AUTH0_CLIENT_SECRET," >> auth0-variables-prod.js
+echo "callbackUrl: $AUTH0_CALLBACK_URL" >> auth0-variables-prod.js
+echo "}" >> auth0-variables-prod.js
