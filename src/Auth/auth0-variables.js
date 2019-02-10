cdebd1f1740d81e06555f7f28c9b9356371e6193
Cael Rowley <caelrowley@gmail.com>
2019-02-10 15:19:48 +0000

    Updated auth0 env config

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackUrl: process.env.AUTH0_CALLBACK_URL
+}
