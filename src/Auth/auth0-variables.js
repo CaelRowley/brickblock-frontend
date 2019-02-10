eb5d16df79cca2e598ba04425b8b8a6dc3d1c96c
Cael Rowley <caelrowley@gmail.com>
2019-02-10 16:19:00 +0000

    Fixed import issue

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: AUTH0_DOMAIN,
-  clientId: AUTH0_CLIENT_ID,
-  clientSecret: AUTH0_CLIENT_SECRET,
-  callbackUrl: AUTH0_CALLBACK_URL
+  domain: 'AUTH0_DOMAIN',
+  clientId: 'AUTH0_CLIENT_ID',
+  clientSecret: 'AUTH0_CLIENT_SECRET',
+  callbackUrl: 'AUTH0_CALLBACK_URL'
 }
