9de02d89603a5bce9d7ecdecf1800a6f6019d827
Cael Rowley <caelrowley@gmail.com>
2019-02-11 01:09:45 +0000

    Fixed updateAuth0Config.sh script

--- scripts/updateAuth0Config.sh
+++ scripts/updateAuth0Config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd ./src/Auth/
-rm -f auth0-variables.js
+rm -f auth0-variables-prod.js
 
-echo "export const AUTH_CONFIG = {" > auth0-variables.js
-echo "domain: '$AUTH0_DOMAIN'," >> auth0-variables.js
-echo "clientId: '$AUTH0_CLIENT_ID'," >> auth0-variables.js
-echo "clientSecret: '$AUTH0_CLIENT_SECRET'," >> auth0-variables.js
-echo "callbackUrl: '$AUTH0_CALLBACK_URL'" >> auth0-variables.js
-echo "}" >> auth0-variables.js
+echo "export const AUTH_CONFIG = {" > auth0-variables-prod.js
+echo "domain: '$AUTH0_DOMAIN'," >> auth0-variables-prod.js
+echo "clientId: '$AUTH0_CLIENT_ID'," >> auth0-variables-prod.js
+echo "clientSecret: '$AUTH0_CLIENT_SECRET'," >> auth0-variables-prod.js
+echo "callbackUrl: '$AUTH0_CALLBACK_URL'" >> auth0-variables-prod.js
+echo "}" >> auth0-variables-prod.js
