dd50dbe91330c73d5e9990445c116f19a8c960c8
Cael Rowley <caelrowley@gmail.com>
2019-02-12 19:28:03 +0000

    Updated data poll

--- scripts/updateMongodbURIProd.sh
+++ scripts/updateMongodbURIProd.sh
@@ -3,4 +3,4 @@
 cd ./src/constants
 
 rm -f MongodbURI.js
-echo "export const MONGODB_URI = '$MONGODB_URI_PROD';" > MongodbURI.js
+echo "export const MONGODB_URI = '$MONGO_URI_PROD';" > MongodbURI.js
